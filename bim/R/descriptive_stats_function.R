22a1be96e08ddeab74194111003599f7297c894e
sacollins34 <sacollins@utexas.edu>
2025-05-02 22:57:54 -0500

    Added titles and names - Stephen

--- bim/R/descriptive_stats_function.R
+++ bim/R/descriptive_stats_function.R
@@ -1,9 +1,11 @@
-#' @description 
-#' This function can be used to calculate different types of descriptive statistics (e.g., number of subjects (n), median, interquartile range (IQR), mean, standard deviation (SD), and the standard error of the mean (SEM)). 
-#' These descriptive statistics are calculated for any group and for any numerical variables of your choosing. 
+#' @title Provides Descriptive Statistics
+#' @name descriptive_data
+#' @description
+#' This function can be used to calculate different types of descriptive statistics (e.g., number of subjects (n), median, interquartile range (IQR), mean, standard deviation (SD), and the standard error of the mean (SEM)).
+#' These descriptive statistics are calculated for any group and for any numerical variables of your choosing.
 #' @param data A data frame or tibble containing a dataset.
 #' @param group_variables A character vector of your column names that is written as "text". This will tell the function how to group the data (e.g., group_variables(c("Sex", "Genotype"))).
-#' @param cell_variables A character vector of column names that is written as "text". This should have the numerical data that you want to summarize (e.g., cell_variables(c("T_Cells", "B_Cells", "Microglia"))). 
+#' @param cell_variables A character vector of column names that is written as "text". This should have the numerical data that you want to summarize (e.g., cell_variables(c("T_Cells", "B_Cells", "Microglia"))).
 #' @param stats A character vector of which descriptive statistics to calculate. Options include: "n", "median", "q1", "q3", "mean", "sd", "sem" and will defaults to all of them if no stats are chosen.
 #' @return A data frame where each row represents a group and each column contains the summary statistics that were calculated.
 #' Each column of descriptive statistics will contain the name of the numerical variable and have the name of the statistic (e.g., T_Cells_mean ).
@@ -13,23 +15,23 @@
 #'   data = data_frame,
 #'   group_variables = c("Sex", "Genotype"),
 #'   cell_variables = c("T_Cells", "B_Cells", "Microglia"),
-#'   stats = c("n", mean", "sd")
+#'   stats = c("n", "mean", "sd")
 #' )
 #' @export
 
 descriptive_data <- function(data, group_variables, cell_variables, stats = c("n", "median", "q1", "q3", "mean", "sd", "sem")) {
-  
+
   stats_formulas <- list( # ~ is needed to create a temporary function ; .x is just a place holder for the column that is being summarized
-    n = sum(!is.na(.x)), # counts the number of subjects per group and does not count NAs 
+    n = sum(!is.na(.x)), # counts the number of subjects per group and does not count NAs
     median = ~median(.x, na.rm = TRUE), # calculates the median
     q1 = ~quantile(.x, 0.25, na.rm = TRUE), # calculates the 25th percentile
     q3 = ~quantile(.x, 0.75, na.rm = TRUE),# calculates the 75th percentile
     mean = ~mean(.x, na.rm = TRUE), # calculates the mean
     sd = ~sd(.x, na.rm = TRUE), # calculates the standard deviation
-    sem = ~sd(.x, na.rm = TRUE) / sqrt(sum(!is.na(.x)))) # calculates the standard error of the mean 
-  
-  selected_formulas <- stats_formulas[stats] # this helps apply each of the stats that were selected or all them if no selection was made below 
-  
+    sem = ~sd(.x, na.rm = TRUE) / sqrt(sum(!is.na(.x)))) # calculates the standard error of the mean
+
+  selected_formulas <- stats_formulas[stats] # this helps apply each of the stats that were selected or all them if no selection was made below
+
   data |> # data table
     group_by(across(all_of(group_variables))) |> # first groups by categorical vaeriables; across() - applies to all columns listed ; all_of() - turns the character string list into column names
     summarise(across( #across() - apply to all columns listed
--- bim/R/outlier_function.R
+++ bim/R/outlier_function.R
@@ -1,40 +1,42 @@
-#' @description 
-#' This function can be used to check for outliers in your dataset for any group and for any numerical variables of your choosing. 
+#' @title Identify Outliers
+#' @name check_for_outliers
+#' @description
+#' This function can be used to check for outliers in your dataset for any group and for any numerical variables of your choosing.
 #' It finds outliers that fall below the first quartile or above the third quartile by more than 1.5 times the interquartile range (IQR).
 #' The method is based on the standard Tukey outlier rule, though the multiplier can be changed to be more conservative or more lenient (e.g., 1, 1.5, 2, 2.5, or 3)
-#' @param data A data frame containing a dataset. 
+#' @param data A data frame containing a dataset.
 #' @param group_variables A character vector of your column names that is written as "text". This will tell the function how to group the data (e.g., group_variables(c("Sex", "Genotype"))).
-#' @param cell_variables A character vector of column names that is written as "text". This should have the numerical data you want to check for outliers (e.g., cell_variables(c("T_Cells", "B_Cells", "Microglia"))). 
+#' @param cell_variables A character vector of column names that is written as "text". This should have the numerical data you want to check for outliers (e.g., cell_variables(c("T_Cells", "B_Cells", "Microglia"))).
 #' @param outlier_condition A numerical value that sets how far from the IQR a value must be to be considered an outlier. Default is 1.5 (Tukey's rule), but it can be adjusted.
-#' @return A data frame containing categorical data, the same columns that were given, and new columns next to each given variable showing which values exactly are ouliers displaying TRUE or FALSE. 
-#' The data frame also only contains the only rows where outliers were detected.                                                                                                                                   
-#' @keywords outliers                                                                                                                                      
+#' @return A data frame containing categorical data, the same columns that were given, and new columns next to each given variable showing which values exactly are ouliers displaying TRUE or FALSE.
+#' The data frame also only contains the only rows where outliers were detected.
+#' @keywords outliers
 #' @examples
 #' check_for_outliers(
 #'   data = my_data,
 #'   group_variables = c("Sex", "Genotype"),
-#'   cell_variables = c("T_Cells", "B_Cells"), 
+#'   cell_variables = c("T_Cells", "B_Cells"),
 #'   outlier_condition = 1.5
 #' )
 #' @export
 
 check_for_outliers <- function(data, group_variables, cell_variables, outlier_condition = 1.5) {
-  outlier_data <- data |> #creating a data frame that is going to check is there is a outlier and add a column that says TRUE or FALSE for each numerical variables given 
+  outlier_data <- data |> #creating a data frame that is going to check is there is a outlier and add a column that says TRUE or FALSE for each numerical variables given
     group_by(across(all_of(group_variables))) |> # first groups by categorical variables; across() - applies to all columns listed ; all_of() - turns the character string list into the column names
     mutate( #creating a new column
       across( #across() - applies to all columns listed
-      .cols = all_of(cell_variables), #grabbing all the numerical variables 
-      .fns = ~ (. < quantile(., 0.25, na.rm = TRUE) - outlier_condition * IQR(., na.rm = TRUE)) | (. > quantile(., 0.75, na.rm = TRUE) + outlier_condition * IQR(., na.rm = TRUE)), # outlier test being applied to each column listed 
+      .cols = all_of(cell_variables), #grabbing all the numerical variables
+      .fns = ~ (. < quantile(., 0.25, na.rm = TRUE) - outlier_condition * IQR(., na.rm = TRUE)) | (. > quantile(., 0.75, na.rm = TRUE) + outlier_condition * IQR(., na.rm = TRUE)), # outlier test being applied to each column listed
       .names = "{.col}_outliers")) #creating the new column to show its an outlier or not
-    
+
   outlier_columns <- paste0(cell_variables, "_outliers") # you need to rewrite columns otherwise it wont let you filter them later
-  
+
   ordered_cell_columns <- as.vector(rbind(cell_variables, outlier_columns)) # this allows for new outlier_column to be next to the original variable
-  
-  categorical_variables <- names(select(data, where(~ is.factor(.) || is.character(.)))) # keeps all the categorical variables to know all of the information about these outliers 
-  
-  # this creates a new data frame that shows only the outliers 
-  outlier_data |> # data frame that was created above 
+
+  categorical_variables <- names(select(data, where(~ is.factor(.) || is.character(.)))) # keeps all the categorical variables to know all of the information about these outliers
+
+  # this creates a new data frame that shows only the outliers
+  outlier_data |> # data frame that was created above
     filter(if_any(all_of(outlier_columns), ~ .)) |> # keeps only rows where outliers are present
     select(all_of(categorical_variables), all_of(ordered_cell_columns))  # keeps only the categorical variables, cell variables, and the new outlier columns
 }
